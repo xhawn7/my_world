fddb84766b4ea5fdd95c54ef7569f1dcb6c853ef
Shawn <shawn@ShawndeMacBook-Pro.local>
2024-06-04 07:19:44 +0800

    update: portfolio

--- app/_components/Experience.tsx
+++ app/_components/Experience.tsx
@@ -19,7 +19,7 @@ export default function Experience() {
       name: '广东财经大学 | 广告学',
       face: '👨‍🎓',
       cardDetailedText:
-        '高中以全校文科第三名数学第一名的成绩进的大学，稀里糊涂选了个装逼专业，莫名其妙把时间都献给了尼采和福柯。校艺术团核心⻣干，就这样被大学上了4年。',
+        '在校期间积极参与各种社团活动，校艺术团核心⻣干，读了很多尼采和福柯，喜欢思考，实习期间在喜马拉雅广告公司（国内4A）负责创维机顶盒相关文案工作。',
       media: {
         name: '广东财经',
         source: {
@@ -29,17 +29,14 @@ export default function Experience() {
       }
     },
     {
-      title: <Content time="2014 - 2021" detail="凤凰网 | 优向 | 司羽" />,
+      title: <Content time="2014 - 2020" detail="上海司羽通信 | 产品经理" />,
       time: '2014 - 2021',
-      name: '凤凰网 | 优向 | 司羽',
+      name: '产品经理',
       face: '👨‍🎨',
-      cardDetailedText: `这段时间是我的职业尝试期。我做了很多产品和新媒体方向的工作，但产品的工作和我的兴趣以及性格不是很匹配，所以在职期间虽然很努力去把它做好可最后也没有达到自己想达到的成绩。主要工作内容如下：
-
-      - 负责凤凰网的客户上海移动和视频军事栏目相关产品工作。实时跟进铁血、米尔等军事类竞品功能迭代，提出差异化解决文案，开展了超过24场线上线下活动。在职期间通过借势、热点跟踪等运营方式让旗下公众号日活提升40%，微博单日转发最高超过2万条。
-      - 负责优向科技 B 端用戶社群 U 点房相关产品工作。对目标用戶进行分级，针对不同用戶群设计公司数据仓库 U-DATA 。同时负责运营公司官方公众号。对u-data的数据进行可视化设计并产出相关数据模型。与客戶开展相关品牌合作运动，包括优向鎏金杯等。
-      - 负责上海电信wap端相关产品工作，对转化率及跳出率负责。在职期间对wap端⻚面进行改版优化，简化下单流程，优化数据埋点，制定相关设计规范等。借势与其他品牌开展联合营销活动，提升了转化率。`,
+      cardDetailedText: `凤凰网外派到移动和视频，负责军事栏目相关运营工作。开展了超过24场线上线下活动。在职期间通过借势、热点跟踪等运营方式让旗下公众号日活提升40%，微博单日转发最高超过2万条。
+      司羽通信外派到上海电信，负责上海电信web端相关产品工作。对转化率及跳出率负责。在职期间对wap端⻚面进行改版优化，简化下单流程，优化数据埋点，制定相关设计规范等。`,
       media: {
-        name: '凤凰网',
+        name: 'ifeng.com',
         source: {
           url: 'https://p3-sdbk2-media.byteimg.com/tos-cn-i-xv4ileqgde/d1c10e334a2e57effd28d49bbdb11168~tplv-xv4ileqgde-resize-w:750.image'
         },
@@ -47,31 +44,32 @@ export default function Experience() {
       }
     },
     {
-      title: <Content time="2021 -2022" detail="上海明品医学 | 前端开发" />,
-      time: '2021 -2022',
-      name: '上海明品医学 | 前端开发',
+      title: <Content time="2021 -2023" detail="上海海钻网络 | 前端开发" />,
+      time: '2021 -2023',
+      name: '前端开发',
       face: '👨‍💻',
-      cardDetailedText:
-        '负责医护平台大专家.com医生端及后台ok系统(PC端)相关前端工作(React16+Antd3), 主要负责加班 🤷🏻‍♂️',
+      cardDetailedText: `负责医护平台大专家.com医生端及后台ok系统(PC端)相关前端工作(React16+Antd3), 主要负责前台活动页面以及后台系统相关前端工作。
+      负责印度信用卡平台Karbon Card前后台3个项目相关前端工作，在职期间每周完成超过3个需求，独立负责后台echarts模块、UI及路由升级、组件库完善、代码重构等工作。`,
       media: {
-        name: '大专家',
+        name: 'Karbon Card',
         source: {
-          url: 'https://ss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/wisegame/wh=512,512/sign=9cb76ea530c79f3d8fb4ec358b93e124/00e93901213fb80e2e664a8d3dd12f2eb9389430.jpg'
+          url: 'https://inc42.com/wp-content/uploads/2022/06/Karbon-Card-Early-Stage-Startup.jpg'
         },
         type: 'IMAGE'
       }
     },
     {
-      title: <Content time="2022 - 2023" detail="上海海钻网络 | 前端开发" />,
-      time: '2022 - 2023',
-      name: '上海海钻网络 | 前端开发',
+      title: <Content time="2023 - 今" detail="自由职业者" />,
+      time: '2023 - 今',
+      name: '自由职业者',
       face: '👨‍💻',
-      cardDetailedText:
-        '负责印度信用卡平台Karbon Card前后台3个项目相关前端工作，在职期间每周完成超过3个需求，独立负责后台echarts模块、UI及路由升级、组件库完善、代码重构等工作。良好的抗压能力及沟通能力，按时完成需求交付，与同事交流分享优秀代码，推动团队学习氛围。',
+      cardDetailedText: `系统学习了前后端及数据库，做了自己的个人网站及一些小项目；
+      每天上油管并用AI进行英文交流，目前可以进行6、7成英文无障碍交流；
+      开通了自己的小红书，经营3个月粉丝破2K。`,
       media: {
         name: 'karbon card',
         source: {
-          url: 'https://inc42.com/wp-content/uploads/2022/06/Karbon-Card-Early-Stage-Startup.jpg'
+          url: 'https://media.licdn.com/dms/image/D5612AQGeJnhyVyXSQw/article-cover_image-shrink_720_1280/0/1679634477414?e=2147483647&v=beta&t=AKZ6CUXc8qK87i4SQJS0Z3692MR8W72VBgiCHLdxSKs'
         },
         type: 'IMAGE'
       }
